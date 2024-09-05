f05cfe9135ad1f4b2e03bc141abf22bfe44a6275
JhAn <s_jehn@naver.com>
2024-09-05 21:05:01 +0900

    20240905

--- DummyProgram/DummyProgram.cpp
+++ DummyProgram/DummyProgram.cpp
@@ -1,8 +1,34 @@
-﻿#include <iostream>
+﻿#include <Windows.h>
+#include <iostream>
+#include <string>
+
+#define CHAR_ARRAY_SIZE 128
 
 int main() {
-    static int varInt = 123456;  // 정적 변수
-    std::cout << "varInt (0x" << std::hex << (uintptr_t)&varInt << std::dec << ") = " << varInt << std::endl;
-    std::cin.get();  // 프로그램이 종료되지 않도록 대기
-    return 0;
+    int varInt = 123456;
+    std::string varString("DefaultString");
+    char arrChar[CHAR_ARRAY_SIZE] = "Long char array right here ->";
+
+    int* ptr2int = (&varInt);
+    int** ptr2ptr = (&ptr2int);
+    int*** ptr2ptr2 = (&ptr2ptr);
+
+
+    do {
+        std::cout << "Process ID: " << std::dec << GetCurrentProcessId() << std::endl;
+        std::cout << "varInt(0x" << std::hex << (uintptr_t)&varInt << std::dec << ") = " << varInt << std::endl;
+        std::cout << "varString(0x" << std::hex << (uintptr_t)&varString << ") = " << varString << std::endl;
+        std::cout << "arrChar[" << std::dec << CHAR_ARRAY_SIZE << "](0x" << std::hex << (uintptr_t)&arrChar << ") = " << arrChar << "\n" << std::endl;
+
+        std::cout << "ptr2int(0x" << std::hex << (uintptr_t)&ptr2int << ") = " << std::hex << "0x" << ptr2int << std::endl;
+        std::cout << "ptr2ptr(0x" << std::hex << (uintptr_t)&ptr2ptr << ") = " << std::hex << "0x" << ptr2ptr << std::endl;
+        std::cout << "ptr2ptr2(0x" << std::hex << (uintptr_t)&ptr2ptr2 << ") = " << std::hex << "0x" << ptr2ptr2 << "\n" << std::endl;
+        
+        std::cout << "Press ENTER to print again." << std::endl;
+        getchar();
+        std::cout << "-----------------------------------------------" << std::endl << std::endl;
+
+    } while (true);
+    
+    return EXIT_SUCCESS;
 }
--- ReadProcessMemory/ReadProcessMemory.cpp
+++ ReadProcessMemory/ReadProcessMemory.cpp
@@ -3,7 +3,12 @@
 
 int main()
 {
-    DWORD processID = 28472; // 더미 프로세스
+    
+    //DWORD processID = 28472; // 더미 프로세스
+
+    DWORD processID = 0;
+    std::cout << "PID: ";
+    std::cin >> std::dec >> processID;
 
 
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
@@ -17,10 +22,15 @@ int main()
     }
 
 
-    LPCVOID baseAddress = (LPCVOID)0x7ff69c59d000; // 더미 프로세스에 실제 존재하는 변수(varInt)의 메모리주소
+    //LPCVOID baseAddress = (LPCVOID)0x7ff69c59d000; // 더미 프로세스에 실제 존재하는 변수(varInt)의 메모리주소
+    uintptr_t baseAddress = 0x0;
+    std::cout << "Memory adress of the integer to read (in hexadecimal): 0x";
+    std::cin >> std::hex >> baseAddress;
+    std::cout << "Reading 0x" << std::hex << baseAddress << "..." << std::endl;
+
     int intRead = 0; // 더미 프로세스에서 ReadProcessMemory로 읽은 변수의 값을 저장할 버퍼
 
-    BOOL rpmReturn = ReadProcessMemory(hProcess, baseAddress, &intRead, sizeof(int), NULL);
+    BOOL rpmReturn = ReadProcessMemory(hProcess, (LPVOID)baseAddress, &intRead, sizeof(int), NULL);
 
     if (rpmReturn == FALSE) {
         std::cout << "ReadProcessMemory failed. GetLastError() = " << std::dec << GetLastError() << std::endl;
@@ -28,9 +38,13 @@ int main()
         return EXIT_FAILURE;
     }
     else if (rpmReturn) {
-        std::cout << "ReadProcessMemory Success.. \nintRead = " << intRead << std::endl;
+        std::cout << "ReadProcessMemory Success.. \nintRead = " << std::dec << intRead << std::endl;
     }
+    
+    std::cout << "Press ENTER to quit." << std::endl;
+    system("pause > nul");
 
+    return EXIT_SUCCESS;
    
    
     
